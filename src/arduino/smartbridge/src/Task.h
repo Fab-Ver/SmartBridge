437b5526be9429b61fae9d6159e2eb9b322b3aac
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-11-19 19:11:48 +0100

    Create interface for general task

---
+++ src/arduino/smartbridge/src/Task.h
@@ -0,0 +1,38 @@
+#ifndef __TASK__
+#define __TASK__
+
+class Task {
+private:
+  int period;
+  int timeElapsed;
+  bool active;
+  
+public:
+  virtual void init(int period){
+    this->period = period;  
+    timeElapsed = 0;
+  }
+
+  virtual void tick() = 0;
+
+  bool updateAndCheckTime(int basePeriod){
+    timeElapsed += basePeriod;
+    if (timeElapsed >= period){
+      timeElapsed = 0;
+      return true;
+    } else {
+      return false; 
+    }
+  }
+
+  bool isActive(){
+    return active;
+  }
+
+  void setActive(bool active){
+    this->active = active;
+  }
+  
+};
+
+#endif
