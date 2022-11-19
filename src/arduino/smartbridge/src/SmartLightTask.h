800ea22bd41b6da2d7f5c56e567cdf2b0501a962
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-11-19 19:12:45 +0100

    Create interface for smart lighting subsystem task

---
+++ src/arduino/smartbridge/src/SmartLightTask.h
@@ -0,0 +1,21 @@
+#ifndef __SMARTLIGHT_TASK__
+#define __SMARTLIGHT_TASK__
+
+#include "Task.h"
+#include "Led.h"
+#include "LightSensorImpl.h"
+#include "MotionSensorImpl.h"
+
+class SmartLightTask : public Task{
+    private:
+        int ledPin, msPin, lsPin;
+        Light* led;
+        LighSensor* ls;
+        MotionSensor* ms;
+        enum {ON, OFF} state;
+    public:
+        SmartLightTask(int ledPin, int lsPin, int msPin);
+        void init(int period);
+        void tick();
+};
+#endif
