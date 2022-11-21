cbb5f4ead9bbffd3fec6c9796c4faff61b558692
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-11-21 16:12:04 +0100

    Add periods consts and fix WLtask init()

--- src/arduino/smartbridge/src/config.h
+++ src/arduino/smartbridge/src/config.h
@@ -12,6 +12,12 @@
 #define SERVO_PIN 0
 #define BUTTON_PIN 0
 
+#define BLINK_PERIOD 0
+#define SMART_LIGHT_PERIOD 0 
+#define WATER_LEVEL_NORMAL_PERIOD 0 
+#define WATER_LEVEL_PREALARM_PERIOD 0
+#define WATER_LEVEL_ALARM_PERIOD 0
+
 typedef enum State { LIGHT_ON, LIGHT_OFF, FSM_OFF} State;
 
 #endif
--- src/arduino/smartbridge/src/main.cpp
+++ src/arduino/smartbridge/src/main.cpp
@@ -8,7 +8,7 @@
 
 
 Scheduler sched;
-Task* wlt;
+WaterLevelTask* wlt;
 
 void buttonHandler(){
   wlt->updateState();
@@ -18,13 +18,13 @@ void setup() {
   sched.init(0);
 
   Task* bt = new BlinkTask(LC_PIN);
-  bt->init(0);
+  bt->init(BLINK_PERIOD);
 
   Task* slt = new SmartLightTask(LA_PIN,PHORES_PIN,PIR_PIN);
-  slt->init(0);
+  slt->init(SMART_LIGHT_PERIOD);
 
-  wlt = new WaterLevelTask(TRIG_PIN,ECHO_PIN,SERVO_PIN,POT_PIN,LB_PIN,LC_PIN,0,slt,bt);
-  wlt->init(0);
+  wlt = new WaterLevelTask(TRIG_PIN,ECHO_PIN,SERVO_PIN,POT_PIN,LB_PIN,LC_PIN,slt,bt);
+  wlt->init(WATER_LEVEL_NORMAL_PERIOD,WATER_LEVEL_PREALARM_PERIOD,WATER_LEVEL_ALARM_PERIOD);
 
   enableInterrupt(BUTTON_PIN,buttonHandler,CHANGE);
 
