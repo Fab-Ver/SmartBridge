b682c3cc8714dbd29f9cbe4bdf305470de90bc0f
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-11-21 10:34:08 +0100

    Create interface for blinking task

---
+++ src/arduino/smartbridge/src/BlinkTask.h
@@ -0,0 +1,22 @@
+#ifndef __BLINK_TASK__
+#define __BLINK_TASK__
+
+#include "Task.h"
+#include "Led.h"
+
+typedef enum BTState { LIGHT_ON, LIGHT_OFF, FSM_OFF} BTState;
+
+class BlinkTask: public Task {
+    private:
+        int pin;
+        Light* led;
+        BTState currState;
+    public:
+        BlinkTask(int pin);  
+        void init(int period);  
+        void tick();
+        void setState(BTState state);
+};
+
+
+#endif
