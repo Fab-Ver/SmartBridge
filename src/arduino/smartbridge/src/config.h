a79c76588eb8add194df4831ff33f68ce525f9a9
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-11-21 14:48:31 +0100

    Create file to store variables and enum

---
+++ src/arduino/smartbridge/src/config.h
@@ -0,0 +1,12 @@
+
+#define LA_PIN 0 
+#define LB_PIN 0
+#define LC_PIN 0
+#define POT_PIN A0
+#define PIR_PIN 0
+#define PHORES_PIN 0 
+#define TRIG_PIN 0
+#define ECHO_PIN 0
+#define SERVO_PIN 0
+
+typedef enum State { LIGHT_ON, LIGHT_OFF, FSM_OFF} State;
