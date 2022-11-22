cdda8da8cb39d690d2b357e20d8f4bc199d765e6
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-11-22 22:41:05 +0100

    Add pins values

--- src/arduino/smartbridge/src/config.h
+++ src/arduino/smartbridge/src/config.h
@@ -1,16 +1,16 @@
 #ifndef __CONFIG__
 #define __CONFIG__
 
-#define LA_PIN 0 
-#define LB_PIN 0
-#define LC_PIN 0
+#define LA_PIN 12
+#define LB_PIN 13
+#define LC_PIN 8
 #define POT_PIN A0
-#define PIR_PIN 0
-#define PHORES_PIN 0 
-#define TRIG_PIN 0
-#define ECHO_PIN 0
-#define SERVO_PIN 0
-#define BUTTON_PIN 0
+#define PIR_PIN 2
+#define PHORES_PIN A1
+#define TRIG_PIN 6
+#define ECHO_PIN 5
+#define SERVO_PIN 4
+#define BUTTON_PIN 7
 
 #define BLINK_PERIOD 0
 #define SMART_LIGHT_PERIOD 0 
