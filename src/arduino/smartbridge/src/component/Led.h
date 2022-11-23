f02b68220128d2e3ce87d53c6034f41ff1f294c8
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-11-23 14:43:39 +0100

    Fix indentation

--- src/arduino/smartbridge/src/component/Led.h
+++ src/arduino/smartbridge/src/component/Led.h
@@ -4,11 +4,11 @@
 #include "Light.h"
 
 class Led : public Light {
-public:
-    Led(int pin);
-    void switchOn();
-    void switchOff();
-private:
-    int pin;
+    public:
+        Led(int pin);
+        void switchOn();
+        void switchOff();
+    private:
+        int pin;
 };
 #endif
