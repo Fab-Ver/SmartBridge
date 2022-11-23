ffd781adc00ea74f6335a9c7db8597f4cea32af8
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-11-23 14:33:17 +0100

    Change int to float

--- src/arduino/smartbridge/src/component/Lcd.h
+++ src/arduino/smartbridge/src/component/Lcd.h
@@ -7,7 +7,8 @@ class Lcd {
     public:
         virtual void on() = 0;
         virtual void off() = 0;
-        virtual void writePreAlarm(String msg, int waterLevel) = 0;
-        virtual void writeAlarm(String msg, int waterLevel, int angle) = 0;
+        virtual void writePreAlarm(String msg, float waterLevel) = 0;
+        virtual void writeAlarm(String msg, float waterLevel, int angle) = 0;
 };
+
 #endif
