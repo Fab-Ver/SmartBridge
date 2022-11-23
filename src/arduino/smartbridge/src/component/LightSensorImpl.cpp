9dffb7ed1615017aa20a2028fd0b9d945038de0c
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-11-23 14:45:41 +0100

    Change class name

--- src/arduino/smartbridge/src/component/LightSensorImpl.cpp
+++ src/arduino/smartbridge/src/component/LightSensorImpl.cpp
@@ -1,11 +1,11 @@
 #include "LightSensorImpl.h"
 #include <Arduino.h>
 
-LighSensorImpl::LighSensorImpl(int pin){
+LightSensorImpl::LightSensorImpl(int pin){
     this->pin = pin;
 }
 
-int LighSensorImpl::getIntensity(){
+int LightSensorImpl::getIntensity(){
     int value = analogRead(pin);
     return map(value,0,1000,0,255);
 };
