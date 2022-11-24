9243893efbfcaf3777f5eb1474f160af842acaf0
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-11-24 17:21:42 +0100

    Add comments

--- src/arduino/smartbridge/src/task/Task.h
+++ src/arduino/smartbridge/src/task/Task.h
@@ -8,13 +8,22 @@ class Task {
     bool active;
   
   public:
+    /*
+      Initialize the task period.
+    */
     virtual void init(int period){
       this->period = period;  
       timeElapsed = 0;
     }
 
+    /*
+      Execute different action according to the actual implemetation.
+    */
     virtual void tick() = 0;
 
+    /*
+      Check if the scheduler can execute the task. 
+    */
     bool updateAndCheckTime(int basePeriod){
       timeElapsed += basePeriod;
       if (timeElapsed >= period){
@@ -25,18 +34,30 @@ class Task {
       }
     }
 
+    /*
+      Return true if the task is active, false otherwise. 
+    */
     bool isActive(){
       return active;
     }
 
+    /*
+      Change the task activity status. 
+    */
     void setActive(bool active){
       this->active = active;
     }
 
+    /*
+      Change task period according to the given argument. 
+    */
     void setPeriod(int period){
       this->period = period;
     }
 
+    /*
+      Perform different action on the task status depending on the actual implementation. 
+    */
     virtual void updateState() = 0;
   
 };
