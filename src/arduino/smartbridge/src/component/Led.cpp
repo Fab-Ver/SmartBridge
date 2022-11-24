8e41308a7fbcf7dc0c4321935c035e61127c3e58
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-11-24 17:00:38 +0100

    Add checks on current state of led

--- src/arduino/smartbridge/src/component/Led.cpp
+++ src/arduino/smartbridge/src/component/Led.cpp
@@ -1,15 +1,23 @@
 #include "Led.h"
 #include <Arduino.h>
 
+int currState = LOW;
+
 Led::Led(int pin){
     this->pin = pin;
     pinMode(pin, OUTPUT);
 }
 
 void Led::switchOn(){
-    digitalWrite(pin,HIGH);
+    if(currState == LOW){
+        digitalWrite(pin,HIGH);
+        currState = HIGH;
+    }
 }
 
 void Led::switchOff(){
-    digitalWrite(pin,LOW);
+    if(currState == HIGH){
+        digitalWrite(pin,LOW);
+        currState = LOW;
+    }
 };
