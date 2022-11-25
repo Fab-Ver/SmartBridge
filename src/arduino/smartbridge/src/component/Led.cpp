cf5d1ca5cd286e9c80e332a9dfa93c80b11b2963
Fabio Veroli <fabio.veroli@studio.unibo.it>
2022-11-25 12:26:54 +0100

    Remove controls

--- src/arduino/smartbridge/src/component/Led.cpp
+++ src/arduino/smartbridge/src/component/Led.cpp
@@ -1,23 +1,15 @@
 #include "Led.h"
 #include <Arduino.h>
 
-int currState = LOW;
-
 Led::Led(int pin){
     this->pin = pin;
     pinMode(pin, OUTPUT);
 }
 
 void Led::switchOn(){
-    if(currState == LOW){
         digitalWrite(pin,HIGH);
-        currState = HIGH;
-    }
 }
 
 void Led::switchOff(){
-    if(currState == HIGH){
         digitalWrite(pin,LOW);
-        currState = LOW;
-    }
 };
